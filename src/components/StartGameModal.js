5b0c76e3e916967abad9b56dffb90ec69245a168
Luke Hook <lukehook@Lukes-MacBook-Pro.local>
2022-05-22 16:03:58 -0400

    Add button to view leaderboard on StartGameModal

--- src/components/StartGameModal.js
+++ src/components/StartGameModal.js
@@ -7,7 +7,10 @@ const StartGameModal = ({ startGame, updateName }) => {
       <input type="text" onChange={updateName} id="user-name" />
       <h2>Find all the characters as quickly as you can!</h2>
       <p>You will be timed when you click start</p>
-      <button onClick={startGame}>Start</button>
+      <div>
+        <button onClick={startGame}>Start</button>
+        <button>Leaderboard</button>
+      </div>
     </div>
   );
 };
