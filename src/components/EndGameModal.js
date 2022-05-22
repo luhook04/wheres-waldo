d80bd3656fd5962d22ddbb383edc0e82f58899d3
Luke Hook <lukehook@Lukes-MacBook-Pro.local>
2022-05-22 14:48:50 -0400

    Edit EndGameModal html

--- src/components/EndGameModal.js
+++ src/components/EndGameModal.js
@@ -3,9 +3,9 @@ import React from "react";
 const EndGameModal = ({ gameResult }) => {
   return (
     <div className="end-container">
-      <h3>
-        {gameResult.username}: {gameResult.secondsTaken} seconds
-      </h3>
+      <h2>Congratulations {gameResult.username}</h2>
+      <h3>Your time was {gameResult.secondsTaken}</h3>
+      <button>View Leaderboards</button>
     </div>
   );
 };
